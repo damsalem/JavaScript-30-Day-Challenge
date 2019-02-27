f515c5fbb5c8b5329a90a745f0ff4379b5311133
damsalem <drummingdan@gmail.com>
2019-02-26 19:01:52 -0500

    Completed the custom video player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]"); // The brackets are used for attributes
 const ranges = player.querySelectorAll(".player__slider");
+const fullScreen = player.querySelector(".fullscreen");
 
 /* Build out functions */
 function togglePlay() {
@@ -21,18 +22,51 @@ function updateButton() {
 }
 
 function skip() {
-  console.log(this.dataset.skip);
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+let isDragging = false;
+
 function handleRangeUpdate() {
-  console.log(this.value);
+  if (!isDragging) return; // Stop the function when they're not mouseDown
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  if (!isDragging) return; // Stop the function when they're not mouseDown
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function bigScreen() {
+  video.requestFullscreen();
+  console.log("Now entering fullscreen mode!");
 }
 
 /* Hook up the event listeners */
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach(button => button.addEventListener("click", skip));
-ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+ranges.forEach(range =>
+  range.addEventListener("mousedown", () => (isDragging = true))
+);
+ranges.forEach(range =>
+  range.addEventListener("mouseup", () => (isDragging = false))
+);
+// Functions same as #52 with range
+// Takes the event XY data and if isDragging
+// Runs scrub() passing in event data
+progress.addEventListener("mousemove", e => isDragging && scrub(e));
+progress.addEventListener("mousedown", () => (isDragging = true));
+progress.addEventListener("mouseup", () => (isDragging = false));
+
+fullScreen.addEventListener("click", bigScreen);
