65ff10c6d85055ff19f9c01d54abfafbbb3d54fd
damsalem <dani@theamsalems.com>
2019-03-29 10:38:05 -0400

    Earlier changes to the webcam project

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,19 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+    .then(localMediaStream => {
+        console.log(localMediaStream);
+        
+//        The following was deprecated
+//        video.src = window.URL.createObjectURL(localMediaStream);
+//        video.play();
+        
+        video.srcObject = localMediaStream;
+        video.play();
+    });
+}
+
+getVideo();
